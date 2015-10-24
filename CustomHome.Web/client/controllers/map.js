c3067dff72f166e9c89c88efda4813b00fc7008f
fleish80 <Fleish.D@gmail.com>
2015-10-24 23:02:56 +0300

    Map

--- CustomHome.Web/client/controllers/map.js
+++ CustomHome.Web/client/controllers/map.js
@@ -3,10 +3,8 @@
 
       $scope.searchbox = {
           template: 'searchbox.tpl.html',
-          //position:'top-right',
-          position: 'top-left',
+          position: 'top-right',
           options: { bounds: {} },
-          //parentdiv:'searchBoxParent',
           events: {
               places_changed: function (searchBox) {
                   places = searchBox.getPlaces()
@@ -29,7 +27,8 @@
               center: {
                   latitude: 31.9522, longitude: 34.8121
               },
-              zoom: 15
+              zoom: 15,
+              bounds: {}
           };
           $scope.markers = [
               { "latitude": 3.9908431028576365, "longitude": -92.86828979738675, "title": "m0", "id": 0 },
@@ -38,15 +37,45 @@
               { "latitude": 3.9908431028576365, "longitude": -92.86828979738675, "title": "m0", "id": 0 }
           ];
 
-          $scope.business = {
-              name: 'המכולת השכונתית',
-              number: '1234567890'
-          };
-
           $scope.defaultBounds = new google.maps.LatLngBounds(
               new google.maps.LatLng(0, 0),
               new google.maps.LatLng(0, 0));
 
           $scope.searchbox.options.bounds = new google.maps.LatLngBounds($scope.defaultBounds.getNorthEast(), $scope.defaultBounds.getSouthWest());
+
+
+          var genRandomMarkers = function (numberOfMarkers, scope) {
+              var markers = [];
+              for (var i = 0; i < numberOfMarkers; i++) {
+                  markers.push(createRandomMarker(i, scope.map.bounds));
+              }
+              scope.map.randomMarkers = markers;
+          };
+
+          var createRandomMarker = function (i, bounds, idKey) {
+              var lat_min = bounds.southwest.latitude,
+                lat_range = bounds.northeast.latitude - lat_min,
+                lng_min = bounds.southwest.longitude,
+                lng_range = bounds.northeast.longitude - lng_min;
+
+              if (!idKey)
+                  idKey = "id";
+
+              var latitude = lat_min + (Math.random() * lat_range);
+              var longitude = lng_min + (Math.random() * lng_range);
+              var ret = {
+                  latitude: latitude,
+                  longitude: longitude,
+                  title: 'm' + i,
+                  icon: '/client/img/poalim-icon.png'
+              };
+              ret[idKey] = i;
+              return ret;
+          };
+
+          $scope.genRandomMarkers = function (numberOfMarkers) {
+              genRandomMarkers(numberOfMarkers, $scope);
+          };
+
       });
   }]);
